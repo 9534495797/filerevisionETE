b35c0d4e2217211eed10bc970fede5aa9be6445a
9534495797 <chaubeyankit927@gmail.com>
2023-01-25 17:49:16 +0530

    ok

---
+++ filesrevision.py
@@ -0,0 +1,21 @@
+# f=open("demo4.txt","w")
+# f.write("i am ankit fchaubey son of mr. dhananjay chaubey")
+
+# f1=open("demo4.txt","w")
+# f1.write("belong to a middle class family")
+
+
+# f1=open("demo4.txt","r")
+# print(f1.read())
+# print(f1.readline())
+
+
+
+f=open("demo4.txt","r")
+print(f.read())
+print(f.tell())
+
+# f=open("demo5.txt","w")
+# f.write("software enginerring from lovely professional university")
+# f=open("demo5.txt")
+# f.close()
